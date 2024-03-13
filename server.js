1536e7d7292e72c9be1e9e5099bbaa3444e9cc60
AropetJ <aropetjoel32@gmail.com>
2024-03-13 20:45:04 +0100

    Changed the port number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import middleWare from './libs/middleWare';
 import router from './routes/index';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 middleWare(app);
 router(app);
